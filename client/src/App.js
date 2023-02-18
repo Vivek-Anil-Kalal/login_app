ff7ce54c2ff34d47e0c1b0c658954fde9918a84d
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-02-18 18:38:16 +0530

    Project SetUp Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { PageNotFound , Username , Profile , Password , Recovery , Register , Reset } from './components'
+
+/* root routes */
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <Username />
+    },
+    {
+        path : "/register",
+        element : <Register />
+    },
+    {
+        path : "/password",
+        element : <Password />
+    },
+    {
+        path : "/profile",
+        element : <Profile />
+    },
+    {
+        path : "/recovery",
+        element : <Recovery />
+    },
+    {
+        path : "/reset",
+        element : <Reset />
+    },
+    {
+        // This path will specify that there is any invalid request then show the page not found component
+        path : "*",
+        element : <PageNotFound />
+    },
+])
 
 const App = () => {
-  return (
-    <div>This is App.js</div>
-  )
+    return (
+        <main>
+            <RouterProvider router={router}>
+
+            </RouterProvider>
+        </main>
+    )
 }
 
 export default App
